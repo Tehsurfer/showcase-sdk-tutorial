0fb787392c73abde10c43506606ec3d6ecb2d0e4
Jesse Khorasanee <jessekhorasanee@gmail.com>
2021-05-14 00:36:42 +1200

    Add bundle to express server

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/bundle', express.static(__dirname + '/bundle'))
+
 app.set('port', process.env.PORT || 8080);
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
